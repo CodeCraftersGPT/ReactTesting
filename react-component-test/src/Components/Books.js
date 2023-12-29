6f6dbaaa6d01c4e75a7310538983a22fac508a78
Code Crafter <kumar@jatanix.com>
2023-12-29 08:08:01 +0530

    booksasyncode

--- react-component-test/src/Components/Books.js
+++ react-component-test/src/Components/Books.js
@@ -1,34 +1,34 @@
-// import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-// const Books = () => {
-//     const [books, setBooks] = useState([]);
-//     const booksApi = 'http://localhost:4000/books';
+const Books = () => {
+    const [books, setBooks] = useState([]);
+    const booksApi = 'http://localhost:4000/books';
 
-//     useEffect(() => {
-//         const fetchData = async () => {
-//             try {
-//                 const response = await fetch(booksApi); // Assuming the books API is defined in the server folder
-//                 const data = await response.json();
-//                 setBooks(data);
-//             } catch (error) {
-//                 console.error('Error fetching books:', error);
-//             }
-//         };
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(booksApi); // Assuming the books API is defined in the server folder
+                const data = await response.json();
+                setBooks(data);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
 
-//         fetchData();
-//     }, []);
+        fetchData();
+    }, []);
 
-//     return (
-//         <div>
-//             <h1 >Books</h1>
+    return (
+        <div>
+            <h1 >Books</h1>
           
-//             <ul>
-//                 {books.map((book) => (
-//                     <li key={book.id}>{book.title}</li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// };
+            <ul>
+                {books.map((book) => (
+                    <li key={book.id}>{book.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
-// export default Books;
+export default Books;
